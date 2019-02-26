63cedb522233fb2ef70a53b522f2c30da88569eb
Humberto Figuera <hfiguera@gmail.com>
2019-02-26 15:32:47 -0600

    Initial commit

---
+++ lib/savvy.ex
@@ -0,0 +1,18 @@
+defmodule Savvy do
+  @moduledoc """
+  Documentation for Savvy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Savvy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
